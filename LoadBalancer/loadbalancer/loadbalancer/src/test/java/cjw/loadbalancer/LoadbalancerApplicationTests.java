f96eb096cec538b6770e7587408fd00c4625d00b
choijaewoo <cchoi00332244@gamil.com>
2024-10-09 18:31:15 +0900

    add loadbalance hw

---
+++ LoadBalancer/loadbalancer/loadbalancer/src/test/java/cjw/loadbalancer/LoadbalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package cjw.loadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
